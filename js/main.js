1c10219fedcdbedff2f63ba6d33471a435bebf9e
homeboys-comics <67285565+homeboys-comics@users.noreply.github.com>
2020-06-22 20:20:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function comics_button_first () {
+    alert("В данный момент ни один комикс недоступен!")
+}
+
+function comics_button_second () {
+    alert("Первый комикс в разработке, следите за новостями в официальной группе!")
+}
